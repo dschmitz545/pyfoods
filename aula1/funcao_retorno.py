6540cbfbcdfa571dcdf07a768b6ce5c56c3c1ce3
Diego Schmitz <diego.sctar@gmail.com>
2022-07-07 01:35:36 -0300

    Revendo um pouco de funcoes

---
+++ aula1/funcao_retorno.py
@@ -0,0 +1,14 @@
+# função void, ou procedure, em algumas linguagens
+
+# void
+def ola():
+    print("Olá")
+
+ola()
+
+# funcao retorno
+def soma(a, b):
+    return (a + b)
+
+result = soma(4,5)
+print(result)
